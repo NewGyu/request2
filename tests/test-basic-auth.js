NewGyu/request2

commit 4171fed7481c1fadf8e2f8f927b4fc349ba5065d

Fix a regression introduced by 76a1333e
Request.prototype.init() should not clear self.method if it is already
set.  This is relevant when retrying requests with proper authentication
(the retries are handled similarly to redirects, but should keep the
same request method).

index.js
@@ -126,7 +126,7 @@ Request.prototype.init = function (options) {
   var self = this
   if (!options) options = {}
   
-  self.method = options.method || 'GET'
+  if (!self.method) self.method = options.method || 'GET'
   
   debug(options)
   if (!self.pool && self.pool !== false) self.pool = globalPool

commit 18311356fc1ae969122faf45baae1bfcf243b0c4

Refactor and expand basic auth tests
The new tests catch the regression corrected by 4171fed7 and the bug
corrected by bd49ad68.

tests/test-basic-auth.js
@@ -24,6 +24,17 @@ var basicServer = http.createServer(function (req, res) {
     res.setHeader('www-authenticate', 'Basic realm="Private"');
   }
 
+  if (req.url == '/post/') {
+    var expectedContent = 'data_key=data_value';
+    req.on('data', function(data) {
+      assert.equal(data, expectedContent);
+      console.log('received request data: ' + data);
+    });
+    assert.equal(req.method, 'POST');
+    assert.equal(req.headers['content-length'], '' + expectedContent.length);
+    assert.equal(req.headers['content-type'], 'application/x-www-form-urlencoded; charset=utf-8');
+  }
+
   if (ok) {
     console.log('request ok');
     res.end('ok');
@@ -36,39 +47,77 @@ var basicServer = http.createServer(function (req, res) {
 
 basicServer.listen(6767);
 
-request({
-  'method': 'GET',
-  'uri': 'http://localhost:6767/test/',
-  'auth': {
-    'user': 'test',
-    'pass': 'testing2',
-    'sendImmediately': false
-  }
-}, function(error, response, body) {
-  assert.equal(response.statusCode, 200);
-  assert.equal(numBasicRequests, 2);
+var tests = [
+  function(next) {
+    request({
+      'method': 'GET',
+      'uri': 'http://localhost:6767/test/',
+      'auth': {
+        'user': 'test',
+        'pass': 'testing2',
+        'sendImmediately': false
+      }
+    }, function(error, res, body) {
+      assert.equal(res.statusCode, 200);
+      assert.equal(numBasicRequests, 2);
+      next();
+    });
+  },
 
-  // If we don't set sendImmediately = false, request will send basic auth
-  request({
-    'method': 'GET',
-    'uri': 'http://localhost:6767/test2/',
-    'auth': {
-      'user': 'test',
-      'pass': 'testing2'
-    }
-  }, function(error, response, body) {
-    assert.equal(response.statusCode, 200);
-    assert.equal(numBasicRequests, 3);
+  function(next) {
+    // If we don't set sendImmediately = false, request will send basic auth
+    request({
+      'method': 'GET',
+      'uri': 'http://localhost:6767/test2/',
+      'auth': {
+        'user': 'test',
+        'pass': 'testing2'
+      }
+    }, function(error, res, body) {
+      assert.equal(res.statusCode, 200);
+      assert.equal(numBasicRequests, 3);
+      next();
+    });
+  },
 
+  function(next) {
     request({
       'method': 'GET',
       'uri': 'http://test:testing2@localhost:6767/test2/'
-    }, function(error, response, body) {
-      assert.equal(response.statusCode, 200);
+    }, function(error, res, body) {
+      assert.equal(res.statusCode, 200);
       assert.equal(numBasicRequests, 4);
-
-      console.log('All tests passed');
-      basicServer.close();
+      next();
     });
-  });
-});
+  },
+
+  function(next) {
+    request({
+      'method': 'POST',
+      'form': { 'data_key': 'data_value' },
+      'uri': 'http://localhost:6767/post/',
+      'auth': {
+        'user': 'test',
+        'pass': 'testing2',
+        'sendImmediately': false
+      }
+    }, function(error, res, body) {
+      assert.equal(res.statusCode, 200);
+      assert.equal(numBasicRequests, 6);
+      next();
+    });
+  }
+];
+
+function runTest(i) {
+  if (i < tests.length) {
+    tests[i](function() {
+      runTest(i + 1);
+    });
+  } else {
+    console.log('All tests passed');
+    basicServer.close();
+  }
+}
+
+runTest(0);